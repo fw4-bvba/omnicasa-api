666bd7a98a665d9a68e7fe44d7bc81888114622c
Sam Feyaerts <sam@fw4.be>
2019-11-06 15:02:53 +0100

    Implement ArrayAccess on ListResponse

--- src/Response/ListResponse.php
+++ src/Response/ListResponse.php
@@ -1,7 +1,7 @@
 <?php
 namespace Omnicasa\Response;
 
-abstract class ListResponse implements \Countable, \IteratorAggregate
+abstract class ListResponse implements \Countable, \IteratorAggregate, \ArrayAccess
 {
     public function __debugInfo(): array
     {
@@ -9,11 +9,37 @@ abstract class ListResponse implements \Countable, \IteratorAggregate
             'count' => $this->count()
         ];
     }
+    
+    abstract public function get(int $position): ResponseObject;
 
     /* IteratorAggregate implementation */
 
     public function getIterator(): ListResponseIterator
     {
         return new ListResponseIterator($this);
+    }
+    
+    /* ArrayAccess implementation */
+    
+    public function offsetExists($offset): bool
+    {
+        if (!is_int($offset)) return false;
+        return $offset < $this->count();
+    }
+    
+    public function offsetGet($offset)
+    {
+        if (!$this->offsetExists($offset)) return null;
+        return $this->get($offset);
+    }
+    
+    public function offsetSet($offset, $value): void
+    {
+        throw new \Exception('offsetSet not implemented on ListResponse');
+    }
+    
+    public function offsetUnset($offset): void
+    {
+        throw new \Exception('offsetUnset not implemented on ListResponse');
     }
 }
