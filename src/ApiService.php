90cf4ec86c11d15e8fda505cec3b6c7a83ff05d5
Sam Feyaerts <sam@fw4.be>
2025-01-28 09:02:10 +0100

    chore: update omnicasa api version

--- src/ApiService.php
+++ src/ApiService.php
@@ -3,8 +3,8 @@ namespace Omnicasa;
 
 final class ApiService
 {
-    const OMNICASA = 'https://newapi.omnicasa.com/1.12/OmnicasaService.svc/';
-    const ADMIN    = 'https://newapi.omnicasa.com/1.12/AdminDBService.svc/';
+    const OMNICASA = 'https://newapi.omnicasa.com/1.13/OmnicasaService.svc/';
+    const ADMIN    = 'https://newapi.omnicasa.com/1.13/AdminDBService.svc/';
 
     private function __construct() {}
 }
